d222b38cb8fcea2ce4468145937e01c215900053
HanhNguyen2102 <88830484+HanhNguyen2102@users.noreply.github.com>
2021-08-12 18:35:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,17 @@
 const CONFIG = {
-    introTitle: 'Này đầu đất',
-    introDesc: `Em thấy nhớ anh, em
-    Em thấy rất nhớ anh thoi
-    Ngày đêm cứ loay hoay
-    Trong suy nghĩ về anh
-    Anh có nhớ em không?
-    Anh có thấy nhớ em không?`,
-    btnIntro: 'Hic hic 🥺🥺',
-    title: 'Anh mà thoát ra coi chừng khỏi cho chơi game đấy 😌😌',
-    desc: 'Anh có yêu em nhiều hongggg 🥺🥺',
-    btnYes: 'Yêu nhiều lắm luôn, muốn ăn luôn 🥺🥺',
-    btnNo: 'Hong, anh yêu bé hàng xóm 😗😗',
-    question:'Vì sao anh lại yêu em dợoooo 🥺🥺',
-    btnReply: 'Gửi cho em nà 😝😝',
-    reply: 'Vì em là em bé của mình anh thoiiii 😘😘',
-    mess: 'Em biết mà, hihi 😚😚',
-    messDesc: 'Vậy thì bớt chơi game lại đi:))',
-    btnAccept: 'Dạ, anh biết gòi. Yêu emmmmmmm 🙆🙆',
+    introTitle: 'Hi mọi người',
+    introDesc: `Cùng chơi trò chơi nhá 😗😗`,
+    btnIntro: 'He he 😝😝',
+    title: 'Mn mà thoát ra là tui đấm á 👊👊',
+    desc: 'Điểm mn ấn tượng ở tui nè 😪😪',
+    btnYes: 'Dễ thương quá chời',
+    btnNo: 'Vì tui hát quá hay 🥲🥲
+',
+    question:'Có thích tui honggg?',
+    btnReply: 'Gửi cho tui nè',
+    reply: 'Đương nhiên gòi, Nguyên tuyệt zời vậy mà 🤭🤭',
+    mess: 'Tui biết mà, mn nói vậy tui ngại lém, hihi 😚😚',
+    messDesc: 'Vậy thì hết dịch dẫn tui đi ăn nha mn 😋😋',
+    btnAccept: 'Ok luôn nè 🙆‍♀️🙆‍♀️',
     messLink: 'http://fb.com' //messages/t/100014015913631
 }
